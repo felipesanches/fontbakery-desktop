072aee664cb3df7c2cc00c59ccce8e785bb43d76
Eli Heuer <eliheuer@gmail.com>
2018-09-30 17:28:50 -0400

    update headline image

--- src/fontbakery-desktop/__main__.py
+++ src/fontbakery-desktop/__main__.py
@@ -5,29 +5,36 @@ import wx
 from wx.lib.floatcanvas import NavCanvas, FloatCanvas
 
 
+def scale_bitmap(bitmap, width, height):
+    image = wx.ImageFromBitmap(bitmap)
+    image = image.Scale(width, height, wx.IMAGE_QUALITY_NORMAL)
+    result = wx.BitmapFromImage(image)
+    return result
+
+
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
         self.CreateStatusBar()
-        splitter = wx.SplitterWindow(self, style=wx.SP_LIVE_UPDATE|wx.SP_3DSASH, size = (1000,500))
+        splitter = wx.SplitterWindow(self, style=wx.SP_LIVE_UPDATE|wx.SP_3DSASH, size = (1024,768))
 
         # Add the Panel
         panel1 = wx.Panel(splitter)
-        panel1.BackgroundColour = 'sky blue'
-        panel1.SetBackgroundColour(wx.LIGHT_GREY)
-        st = wx.StaticText(panel1, -1, 'DRAG FONTS HERE', (15,10))
-        st.SetFont(wx.FFont(32, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
+        panel1.BackgroundColour = 'GREY'
+        # panel1.SetBackgroundColour(wx.LIGHT_GREY)
+        #st = wx.StaticText(panel1, -1, 'DRAG FONTS HERE', (15,10))
+        #st.SetFont(wx.FFont(32, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
 
-        bmp = wx.Bitmap(os.path.join(os.path.dirname(__file__), 'headline.png'))
-        sb = wx.StaticBitmap(panel1, bitmap=bmp, pos=(15,85))
-
+        bitmap = wx.Bitmap(os.path.join(os.path.dirname(__file__), 'headline.png'))
+        bitmap = scale_bitmap(bitmap, 512, 512)
+        sb = wx.StaticBitmap(panel1, bitmap=bitmap, pos=(0,0))
 
         # Add the Canvas
         panel2 = NavCanvas.NavCanvas(splitter, BackgroundColor = "DARK GREY")
         Canvas = panel2.Canvas
 
         # set up the Splitter
-        sash_Position = 500
+        sash_Position = 512
         splitter.SplitVertically(panel1, panel2, sash_Position)
         min_Pan_size = 0
         splitter.SetMinimumPaneSize(min_Pan_size)
@@ -42,5 +49,6 @@ class MyApp(wx.App):
         return True
 
 
-app = MyApp(0)
-app.MainLoop()
+if __name__ == '__main__':
+    app = MyApp(0)
+    app.MainLoop()
