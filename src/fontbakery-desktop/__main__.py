b7f30f6d81f9fb31e6dfaca31ce14ccc1afc0d77
Eli Heuer <eliheuer@gmail.com>
2018-09-30 18:54:22 -0400

    run pycodestyle on __main__.py

--- src/fontbakery-desktop/__main__.py
+++ src/fontbakery-desktop/__main__.py
@@ -16,52 +16,48 @@ class MyFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
         self.CreateStatusBar()
-        splitter = wx.SplitterWindow(self, style=wx.SP_LIVE_UPDATE|wx.SP_3DSASH, size = (1024,768))
+        splitter = wx.SplitterWindow(self,
+                                     style=wx.SP_LIVE_UPDATE | wx.SP_3DSASH,
+                                     size=(1024, 768))
 
         # Add the Panel
         panel1 = wx.Panel(splitter)
         panel1.BackgroundColour = 'GREY'
-        # panel1.SetBackgroundColour(wx.LIGHT_GREY)
-        #st = wx.StaticText(panel1, -1, 'DRAG FONTS HERE', (15,10))
-        #st.SetFont(wx.FFont(32, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
-
-        bitmap = wx.Bitmap(os.path.join(os.path.dirname(__file__), 'headline.png'))
+        bitmap = wx.Bitmap(os.path.join(os.path.dirname(__file__),
+                           'headline.png'))
         bitmap = scale_bitmap(bitmap, 512, 512)
-        sb = wx.StaticBitmap(panel1, bitmap=bitmap, pos=(0,0))
+        sb = wx.StaticBitmap(panel1, bitmap=bitmap, pos=(0, 0))
 
-        # add the Canvas
+        # Add the Canvas
         panel2 = NavCanvas.NavCanvas(splitter,
-                                     ProjectionFun = None,
-                                     Debug = 0,
-                                     BackgroundColor = "LIGHT GREY",
+                                     ProjectionFun=None,
+                                     Debug=0,
+                                     BackgroundColor="LIGHT GREY",
                                      )
         Canvas = panel2.Canvas
 
-        # put something on the Canvas
-        Point = (15,10)
+        # Put stuff on the Canvas
+        Point = (15, 10)
         Canvas.AddScaledTextBox("Markdown output \nwill be shown here.",
-                                      Point,
-                                      2,
-                                      Color = "DARK GREY",
-                                      BackgroundColor = None,
-                                      LineColor = "DARK GREY",
-                                      LineStyle = "Solid",
-                                      LineWidth = 1,
-                                      Width = None,
-                                      PadSize = 5,
-                                      Family = wx.SWISS,
-                                      Style = wx.NORMAL,
-                                      Weight = wx.NORMAL,
-                                      Underlined = False,
-                                      Position = 'br',
-                                      Alignment = "center",
-                                      InForeground = False)
-
+                                Point,
+                                2,
+                                Color="DARK GREY",
+                                BackgroundColor=None,
+                                LineColor="DARK GREY",
+                                LineStyle="Solid",
+                                LineWidth=2,
+                                Width=None,
+                                PadSize=5,
+                                Family=wx.SWISS,
+                                Style=wx.NORMAL,
+                                Weight=wx.NORMAL,
+                                Underlined=False,
+                                Position='br',
+                                Alignment="center",
+                                InForeground=False)
         wx.CallAfter(Canvas.ZoomToBB)
 
- 
-
-        # set up the Splitter
+        # Set up the Splitter
         sash_Position = 512
         splitter.SplitVertically(panel1, panel2, sash_Position)
         min_Pan_size = 0
@@ -71,13 +67,12 @@ class MyFrame(wx.Frame):
 
 class MyApp(wx.App):
     def OnInit(self):
-        frame = MyFrame(None, title='fontbakery')
+        frame = MyFrame(None, title='FontBakery')
         frame.Show(True)
         self.SetTopWindow(frame)
         return True
 
 
-#if __name__ == '__main__':
-app = MyApp(0)
-app.MainLoop()
-
+if __name__ == '__main__':
+    app = MyApp(0)
+    app.MainLoop()
